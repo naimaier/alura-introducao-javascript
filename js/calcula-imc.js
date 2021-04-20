87412a320dfb084f64c76651770d3fb4d817f9d5
Henrique Naimaier <coubron@yahoo.com.br>
2021-04-20 14:22:27 -0300

    Refactoring code

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -32,9 +32,17 @@ for (i = 0; i < pacientes.length; i++) {
     }
     
     if (pesoEhValido && alturaEhValida) {
-        var imc = peso / (altura * altura)
-        tdImc.textContent = imc.toFixed(2)
+        var imc = calculaImc(peso, altura)
+        tdImc.textContent = imc
     } else {
         tdImc.textContent = 'Altura e/ou peso inválidos!'
     }
+}
+
+function calculaImc(peso, altura) {
+    var imc = 0
+
+    imc = peso / (altura * altura)
+
+    return imc.toFixed(2)
 }
