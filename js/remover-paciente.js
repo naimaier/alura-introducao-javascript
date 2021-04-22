95fcbc05314b30f48508ac71a2da71370c5bf8c4
Henrique Naimaier <coubron@yahoo.com.br>
2021-04-22 11:38:46 -0300

    Adding animation before removing patient

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,7 +1,8 @@
 var table = document.querySelector('table')
 
 table.addEventListener('dblclick', event => {
-    var alvoDoEvento = event.target //TD
-    var paiDoAlvo = alvoDoEvento.parentNode //TR
-    paiDoAlvo.remove()
+    event.target.parentNode.classList.add('fadeOut')
+    setTimeout(function(){
+        event.target.parentNode.remove()
+    }, 500)
 })
